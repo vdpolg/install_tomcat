f537b07562078591f0e07073656f99a87ad24155
vdpolg <vdpolg@gmail.com>
2018-09-01 01:02:24 +0800

    AP多站台完成,強化防呆機制

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # install_tomcat
-install tomcat
+AP:	create_ap_tomcat.sh
+WEB:	install tomcat.sh
+Test:	test
 <pre>
-名稱           版本    日期            作者    備註
-安裝tomcat     v1      20180830        arthur
-               v1.1    20180830        arthur  全新改款 tomcat_ui
-               v1.2    20180831        arthur  強化重複安裝檢核
-
+名稱           版本	日期            作者    備註
+安裝tomcat     v1	20180830        arthur
+               v1.1	20180830        arthur  全新改款 tomcat_ui
+               v1.2	20180831        arthur  強化重複安裝檢核
+               v1.3	20180901        arthur  web ap 拆開，ap 多站台防呆
+						待辦：web、各項啟動shell
 </pre>
--- create_ap_tomcat.sh
+++ create_ap_tomcat.sh
@@ -2,77 +2,52 @@
 #名稱           	版本    日期            作者    備註
 #安裝AP tomcat站台	v1.2    20180831        arthur  web 和ap 拆成2個shell
 
-WRK_PATH=$PWD
-DAT=$(date +%Y%m%d-%H%M%S)
+WRK_PATH=$PWD #工作路徑
+DAT=$(date +%Y%m%d-%H%M%S) #timestamp 暫時用不到
+TOMC="apache-tomcat-8.0.53.tar.gz" #tomcat 安裝檔
+TOM="$(echo $TOMC | sed s/.tar.gz//)" #tomcat 目錄名稱
 # 多站台設定(最多3組站台)
-# 站台名稱
-APName=$1
-# 站台數量 0~2
-APCount=$2
-# example 
-#請輸入$1 $2 變數
-if [[ $# == 0 ]] ; then  
- echo "請輸入\$1 \$2 變數"
- echo "Usage command --> : $0 <ap name> <0~2>"
- echo "安裝中止！"
- exit 1
-else
- echo -n "First Installing" ;sleep 1
- echo -n " ." ;sleep 1
- echo -n "." ;sleep 1
- echo "."
- echo 
-fi 
+APName=$1	# 站台名稱
+APCount=$2	# 站台序號 0~2
+function var2(){ #變數只能2個且序號介於0~2
+  echo "請輸入<站台名稱>及<站台序號> 0~2"
+  echo "Usage command --> : $0 <ap name> <0~2>"
+  echo -e "安裝中止！ \n"
+  exit 1
+}
 
-if [[ -d `ls -d /opt/apache-tomcat` ]] 2> /dev/null ; then #1 check
-echo -n "已存在 `ls -d /opt/apache-tomcat` 是否繼續安裝？(y/n) "  ;read YN
-  if [[ $YN = Y || $YN = y ]]; then #2
-  echo -e "OK!繼續安裝 \n"
-  cd /opt ;rm -rf apache-tomcat 
-  TOMC="apache-tomcat-8.0.53.tar.gz"
-  echo '========安裝Tomcat 8.0.53========='
-  sleep 1
-  cd $WRK_PATH
-  sha512sum -c ${TOMC}.sha512 > /dev/null
-    if [[ `echo $?` == 0 ]] ; then #3
-    echo "驗證成功，繼續安裝"
-    cd $WRK_PATH ;tar zxvf $TOMC 
-    cp -pr $(echo $TOMC | sed s/.tar.gz//) /opt/apache-tomcat
-    chown -R proap:proap /opt/apache-tomcat
+if [[ $# != 2 ]] ; then  #1 check 變數只能2個
+var2
+elif [[ $APCount != 0 ]] && [[ $APCount != 1 ]] && [[ $APCount != 2 ]]; then #1 check 站台序號只能 0~2 
+var2
+else #1
+  echo -e "Installing" ;sleep 3
 
-    #建AP目錄
-    mkdir -p /ap/${APName} ;
-
-    #複製tomcat檔案
-    cp -pr $(echo $TOMC | sed s/.tar.gz//)/{conf,temp,work,webapps} -t /ap/${APName}/
-    chown -R proap:proap /ap/${APName}
-
-    #建log的soft link
-	  if [[ -d /log/${APName} ]] ; then
-	  echo -en "==logs folder 已存在," ; sleep 1
-	  echo -e "故/ap/${APName}/logs 維持不變== \n"
-	  else
-	  mkdir -p /log/${APName}
-	  ln -sf /log/${APName} /ap/${APName}/logs
-	  chown -R proap:proap /log/${APName}
-	  fi
-	
-	else #3
-	echo "驗證失敗，請洽系統人員!"
-	exit 1
-        
-	fi #3
-                rm -rf $WRK_PATH/$(echo $TOMC | sed s/.tar.gz//)
-                echo -e "==Tomcat install Success!== \n"
-				
-# xml port 設定 
-Serv_P=8005
-Conn_P=8080
-AJP_P=8009
-Redi_P=8443
-
+  if [[ -d `ls -d /opt/apache-tomcat` ]] 2> /dev/null ; then #2 若tomcat 存在就install 
+    cd /opt ;rm -rf apache-tomcat #清除舊檔
+    mkdir -p /opt/apache-tomcat #建立新目錄
+    echo -e '========安裝$(echo $TOMC | sed s/.tar.gz//)========= \n'
+    sleep 1
+    cd $WRK_PATH
+    sha512sum -c ${TOMC}.sha512 > /dev/null
+      if [[ `echo $?` == 0 ]] ; then #3 checksum 安裝檔有效性
+        echo "驗證成功，繼續安裝"
+	    #解tar 並copy & rename至/opt
+        tar zxvf $TOMC 
+	cd $TOM; cp -pr $(ls|egrep -v '(logs|webapps|work|conf|temp)') /opt/apache-tomcat
+        chown -R proap:proap /opt/apache-tomcat 
+	mkdir -p /ap/${APName} #建AP目錄
+        cd $WRK_PATH ; cp -pr $TOM/{conf,temp,work,webapps} -t /ap/${APName}/  #複製tomcat檔案
+        chown -R proap:proap /ap/${APName}
+	rm -rf $WRK_PATH/$TOM #裝完移除資料
+		
+	# server.xml port 設定 
+	Serv_P=8005
+	Conn_P=8080
+	AJP_P=8009
+	Redi_P=8443
 case $APCount in
-0)echo "預設值0"
+0)echo -e "\n ====  站台名稱：${APName} ，這是第${APCount}站台(預設值) ====\n"
 echo    "================  Now port setting  =================="
 echo -e "================  Server_Port=$Serv_P  ================"
 echo -e "================  Connect_Port=$Conn_P  ==============="
@@ -83,8 +58,7 @@ do
 grep --color=always $G /ap/${APName}/conf/server.xml
 done
 ;;
-1)echo "你已輸入1(在預設值+10)"
-echo $Serv_P |awk '{print $1+10}'
+1)echo -e "\n ====  站台名稱：${APName} ，這是第${APCount}站台(預設port+10) ====\n"
 echo    "================  Now port setting   =================="
 echo -e "================  Server_Port  =  `echo $Serv_P|awk '{print $1+10}'`  ==============="
 echo -e "================  Connect_Port =  `echo $Conn_P|awk '{print $1+10}'`  ==============="
@@ -96,8 +70,7 @@ sed -i s/$G/`echo $G |awk '{print $1+10}'`/g /ap/${APName}/conf/server.xml
 grep --color=always `echo $G |awk '{print $1+10}'` /ap/${APName}/conf/server.xml
 done
 ;;
-2)echo "你已輸入2(預設值+20)"
-echo $Serv_P |awk '{print $1+20}'
+2)echo -e "\n ====  站台名稱：${APName} ，這是第${APCount}站台(預設port+20) ====\n"
 echo    "================  Now port setting   =================="
 echo -e "================  Server_Port  =  `echo $Serv_P|awk '{print $1+20}'`  ==============="
 echo -e "================  Connect_Port =  `echo $Conn_P|awk '{print $1+20}'`  ==============="
@@ -108,26 +81,37 @@ do
 sed -i s/$G/`echo $G |awk '{print $1+20}'`/g /ap/${APName}/conf/server.xml
 grep --color=always `echo $G |awk '{print $1+20}'` /ap/${APName}/conf/server.xml
 done
-
 ;;
-*) echo 請輸入0~2的數字;;
+*) echo "請輸入站台序號(0~2)" # 有 var2 判斷，這邊應該用不到XD
+   echo -e "待確認後再執行，離開安裝程式! \n"
+   exit 1 ;;
 esac
- 
-  echo -e "待確認後再執行，離開安裝程式! \n"
+	  if [[ -d /log/${APName} ]] ; then #4 建log的soft link
+	    echo -en "==logs folder 已存在," ; sleep 1
+	    echo -e "故/ap/${APName}/logs 維持不變== \n"
+	  else #4
+	    mkdir -p /log/${APName}
+	    ln -sf /log/${APName} /ap/${APName}/logs
+	    chown -R proap:proap /log/${APName}
+	  fi #4	
+		
+	else #3 tomcat.tar.gz checksum
+	  echo "驗證失敗，請洽系統人員!"
+	  exit 1        
+	fi #3
+	
+  else #2 test tomcat exist
+  echo -e "tomcat目錄不存在，請洽系統人員! \n"
   exit 1
+  fi #2
+  
+fi #1 check APName and AP_NO
 
-else #2
-echo "程式中止!"
-fi #2
-else #1
-echo "尚未安裝Tomcat,程式中止！"
-fi #1
-
+echo -e "==============  Install Success!  ===============\n"
+exit 0
 #剩文件step7 ......
 
 #安裝JavaJDK
 #cd /soures/software
 #rpm -ivh jdk-7u79-linux-x64.rpm
 #java -version
-
-
